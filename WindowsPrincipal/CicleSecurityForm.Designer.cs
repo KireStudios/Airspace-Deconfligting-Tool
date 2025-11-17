7134e2ad36e716503a1947e0c9633b999a4f84f8
KireStudios <erik.ventura.gili@gmail.com>
2025-11-17 20:02:43 +0100

    Art add cicles i security

--- WindowsPrincipal/CicleSecurityForm.Designer.cs
+++ WindowsPrincipal/CicleSecurityForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CicleSecurityForm));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -41,35 +42,39 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(112, 50);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 49);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(701, 40);
+            this.label1.Size = new System.Drawing.Size(556, 39);
             this.label1.TabIndex = 0;
             this.label1.Text = "Add Security Distance and number of Cicles";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(196, 155);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(79, 198);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(133, 20);
+            this.label2.Size = new System.Drawing.Size(162, 28);
             this.label2.TabIndex = 1;
             this.label2.Text = "Security Distance";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(564, 155);
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(381, 198);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(129, 20);
+            this.label3.Size = new System.Drawing.Size(156, 28);
             this.label3.TabIndex = 2;
             this.label3.Text = "Number of Cicles";
             // 
             // SecurityTextBox
             // 
-            this.SecurityTextBox.Location = new System.Drawing.Point(151, 230);
+            this.SecurityTextBox.Location = new System.Drawing.Point(101, 230);
             this.SecurityTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.SecurityTextBox.Name = "SecurityTextBox";
             this.SecurityTextBox.Size = new System.Drawing.Size(223, 26);
@@ -77,7 +82,7 @@
             // 
             // CicleTextBox
             // 
-            this.CicleTextBox.Location = new System.Drawing.Point(515, 230);
+            this.CicleTextBox.Location = new System.Drawing.Point(403, 230);
             this.CicleTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.CicleTextBox.Name = "CicleTextBox";
             this.CicleTextBox.Size = new System.Drawing.Size(223, 26);
@@ -85,33 +90,50 @@
             // 
             // AddButton
             // 
+            this.AddButton.AutoSize = true;
+            this.AddButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.AddButton.BackColor = System.Drawing.Color.Transparent;
             this.AddButton.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.AddButton.Location = new System.Drawing.Point(407, 324);
+            this.AddButton.FlatAppearance.BorderSize = 0;
+            this.AddButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.AddButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.AddButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddButton.Font = new System.Drawing.Font("Monotype Corsiva", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddButton.Location = new System.Drawing.Point(686, 207);
             this.AddButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.AddButton.Name = "AddButton";
-            this.AddButton.Size = new System.Drawing.Size(84, 40);
+            this.AddButton.Size = new System.Drawing.Size(78, 49);
             this.AddButton.TabIndex = 5;
             this.AddButton.Text = "Add";
-            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.UseVisualStyleBackColor = false;
             this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
             // 
             // CloseButton
             // 
+            this.CloseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.CloseButton.AutoSize = true;
+            this.CloseButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.CloseButton.BackColor = System.Drawing.Color.Transparent;
             this.CloseButton.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.CloseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CloseButton.Location = new System.Drawing.Point(397, 465);
+            this.CloseButton.FlatAppearance.BorderSize = 0;
+            this.CloseButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.CloseButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseButton.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CloseButton.ForeColor = System.Drawing.Color.DarkRed;
+            this.CloseButton.Location = new System.Drawing.Point(945, 595);
             this.CloseButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.CloseButton.Name = "CloseButton";
-            this.CloseButton.Size = new System.Drawing.Size(105, 42);
+            this.CloseButton.Size = new System.Drawing.Size(51, 54);
             this.CloseButton.TabIndex = 6;
-            this.CloseButton.Text = "Return";
-            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Text = "X";
+            this.CloseButton.UseVisualStyleBackColor = false;
             this.CloseButton.Click += new System.EventHandler(this.ReturnButton_Click);
             // 
             // AutoFill
             // 
             this.AutoFill.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.AutoFill.Location = new System.Drawing.Point(541, 324);
+            this.AutoFill.Location = new System.Drawing.Point(366, 573);
             this.AutoFill.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.AutoFill.Name = "AutoFill";
             this.AutoFill.Size = new System.Drawing.Size(101, 40);
@@ -124,7 +146,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1024, 672);
             this.Controls.Add(this.AutoFill);
             this.Controls.Add(this.CloseButton);
             this.Controls.Add(this.AddButton);
@@ -133,13 +157,13 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "CicleSecurityForm";
             this.Text = "CicleSecurityForm";
             this.Load += new System.EventHandler(this.CicleSecurityForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
 
         #endregion
